c0cd58343d39758d67ecbf5bd350111f617b5fbe
i.shatunova <ivashatunova@gmail.com>
2023-05-15 18:07:31 +0300

    Fix assect balance

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -2,17 +2,14 @@ package ru.netology.web.test;
 
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
-import ru.netology.web.page.LoginPageV1;
-import ru.netology.web.page.LoginPageV2;
-import ru.netology.web.page.LoginPageV3;
-import ru.netology.web.page.TransferPage;
+import ru.netology.web.page.*;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
     @Test
-    void shouldTransferMoneyBetweenOwnCardsV1() {
+    void shouldTransferMoneyBetweenOwnCards1from2() {
       open("http://localhost:9999");
       var loginPage = new LoginPageV1();
       var transferInfo = new DataHelper.TransferInfo(DataHelper.card2Info(), 1000);
@@ -21,34 +18,44 @@ class MoneyTransferTest {
       var verificationPage = loginPage.validLogin(authInfo);
       var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
       var dashboardPage = verificationPage.validVerify(verificationCode);
+
+      var balanceCard1 = dashboardPage.getCardBalance(DataHelper.card1Info());
+      var balanceCard2 = dashboardPage.getCardBalance(DataHelper.card2Info());
+
       var transferPage = dashboardPage.selectCardTo(DataHelper.card1Info());
       var transfer = transferPage.transferMoney(transferInfo);
       var balance1 = transfer.getCardBalance(DataHelper.card1Info());
       var balance2 = transfer.getCardBalance(DataHelper.card2Info());
-      assertEquals(5000,balance1);
-      assertEquals(5000,balance2);
+
+      assertEquals(balanceCard1 + 1000,balance1);
+      assertEquals(balanceCard2 - 1000,balance2);
 
     }
 
   @Test
-  void shouldTransferMoneyBetweenOwnCardsV2() {
+  void shouldTransferMoneyBetweenOwnCards2from1() {
     open("http://localhost:9999");
-    var loginPage = new LoginPageV2();
-//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
+    var loginPage = new LoginPageV1();
+    var transferInfo = new DataHelper.TransferInfo(DataHelper.card1Info(), 1000);
+//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
     var authInfo = DataHelper.getAuthInfo();
     var verificationPage = loginPage.validLogin(authInfo);
     var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
+    var dashboardPage = verificationPage.validVerify(verificationCode);
+
+    var balanceCard1 = dashboardPage.getCardBalance(DataHelper.card1Info());
+    var balanceCard2 = dashboardPage.getCardBalance(DataHelper.card2Info());
+
+    var transferPage = dashboardPage.selectCardTo(DataHelper.card2Info());
+    var transfer = transferPage.transferMoney(transferInfo);
+    var balance1 = transfer.getCardBalance(DataHelper.card1Info());
+    var balance2 = transfer.getCardBalance(DataHelper.card2Info());
+
+    assertEquals(balanceCard1 - 1000,balance1);
+    assertEquals(balanceCard2 + 1000,balance2);
+
   }
 
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV3() {
-    var loginPage = open("http://localhost:9999", LoginPageV3.class);
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
 
 }
 
